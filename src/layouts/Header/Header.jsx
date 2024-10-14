994768992f8c99b106a994494179697e42a89b2b
agus <agusalta002@gmail.com>
2024-10-14 14:55:46 -0300

    Add Componente Header - Basic

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./header.css";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+
+function Header() {
+  return (
+    <header id="header-container">
+      <div className="top">
+        <div className="top-left">
+          <h1 className="top-left_title">Campo 1</h1>
+          <FileCopyIcon fontSize="large" style={{ color: "#0c154d" }} />
+          <p className="top-left_state">Ok</p>
+          <p className="top-left_info">Total de granos: 200 (ton)</p>
+        </div>
+        <div className="top-right">
+          <button className="top-right_btn">Editar establecimiento</button>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="bottom-left">
+          <button className="bottom-left_btn">Principal</button>
+          <button className="bottom-left_btn">Bitácora</button>
+        </div>
+        <div className="bottom-right">
+          <a className="bottom-right_a" href="/">
+            Cotizá tu seguro
+          </a>
+          <a className="bottom-right_a" href="/">
+            Financiate con tus granos
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
