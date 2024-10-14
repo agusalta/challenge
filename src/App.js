4e6d99ea7590a23109c8344de45bca890482fd12
agus <agusalta002@gmail.com>
2024-10-14 14:55:20 -0300

    Add layout al proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from "./layouts/Header/Header"
+import Chart from "./layouts/Chart/Chart"
+import Maps from "./layouts/Maps/Maps"
+
 
 function App() {
   return (
-    <div className="App">
-      Hola
-    </div>
+    <main id="app-container">
+      <Header />
+      <div id='content-container'>
+        <Chart />
+        <Maps />
+      </div>
+    </main>
   );
 }
 
